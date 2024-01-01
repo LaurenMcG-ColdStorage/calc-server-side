d7885d186ddf77bbc602ff4fe49a6bb471513c63
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2023-12-31 21:19:07 -0500

    All base functionality complete. Completed code comments

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,34 +2,42 @@ console.log('client.js is sourced!');
 
 let currentComputation = {numOne: '', numTwo: '', operator: ''};
 
-function addOperator(event){
-    event.preventDefault();
-    currentComputation.operator = event.target.value;
-    console.log(currentComputation.operator);
+function clearInputs(event){                     //Create function to clear inputs
+    const firstDigit = document.querySelector('.firstNumber');     //Find first location
+    const secondDigit = document.querySelector('.secondNumber');   //Find second location
+    firstDigit.value = '';            //Clear first
+    secondDigit.value = '';           //Clear Second
+    currentComputation.operator = ''; //Clear operator
 }
 
-function requestMath(event){
+function addOperator(event){
     event.preventDefault();
-    console.log('Sending calculation request to server');
-    const firstDigit = document.querySelector('.firstNumber');
-    const secondDigit = document.querySelector('.secondNumber');
-    currentComputation.numOne = firstDigit.value;
-    currentComputation.numTwo = secondDigit.value;
-    axios({
+    currentComputation.operator = event.target.value;  //Assign object property with clicked operator
+    console.log(currentComputation.operator);          //Validate
+}
+
+function requestMath(event){                          //Package inputs to be sent to server
+    event.preventDefault();
+    console.log('Sending calculation request to server'); //Validate function initiation
+    const firstDigit = document.querySelector('.firstNumber'); //Find first digit location
+    const secondDigit = document.querySelector('.secondNumber');  //Find second digit location
+    currentComputation.numOne = firstDigit.value;   //Grab first digit, store in object
+    currentComputation.numTwo = secondDigit.value;  //Grab second digit, store in object
+    axios({                            //Send packaged object to server
         method: 'POST',
         url: '/calculations',
         data: currentComputation
     })
     .then((response) => {
-        console.log(response);
-        renderData(response);
+        console.log(response);         //Validate server response
+        renderData(response);          //Update page
     })
     .catch((error) => {
         console.log(error);
     })
 };
 
-function renderData(){
+function renderData(){                //Update page
     axios({
         method: 'GET',
         url: '/calculations'
@@ -46,6 +54,9 @@ function renderData(){
             historyCalc.innerHTML +=`<li>${calculations[calc].numOne} ${calculations[calc].operator} ${calculations[calc].numTwo} = ${calculations[calc].result}</li>`
         };
     })
-    
+    .catch((error) => {
+        console.log(error);
+    });
 };
+
 renderData();
